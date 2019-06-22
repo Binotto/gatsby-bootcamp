1687724de8adebda189c7b4aec251d219fcee483
Matheus <Binotto>
2019-06-22 17:28:43 -0300

    Syling Gatsby with CSS Modules

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ const ContactPage = () => {
     return (
         <Layout>
             <h1>Contact Me</h1>
-            <p>The best way to reach me is via <a href="https://www.linkedin.com/in/matheus-binotto-a51787143/" target="_blank">My Linkedin</a></p>
+            <p>The best way to reach me is via <a href="https://www.linkedin.com/in/matheus-binotto-a51787143/" target="_blank" rel="noopener noreferrer">My Linkedin</a></p>
         </Layout>
     )
 }
