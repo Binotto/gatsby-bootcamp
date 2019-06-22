0de1c2ab3e11f399daa1ce67f21c3309294fed4f
Matheus <Binotto>
2019-06-21 21:33:18 -0300

    Working with Gatsby Pages II

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact Me</h1>
+            <p>The best way to reach me is via e-mail matheus.binotto@hotmail.com</p>
+        </div>
+    )
+}
+
+export default ContactPage
