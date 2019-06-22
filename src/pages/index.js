e4f6a6f9401a2f71f08b2120d82a09cda97ac418
Matheus <Binotto>
2019-06-21 21:06:15 -0300

    Working with Gatsby Pages I

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Hello World!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello guys</h1>
+            <h2>I'm Matheus Binotto, a Full Stack Web Developer.</h2>
+        </div>
+    )
+}
+
+export default IndexPage
